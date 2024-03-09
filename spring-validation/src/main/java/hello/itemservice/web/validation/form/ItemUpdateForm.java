8b9cd03c3abf11f809a0c03b703a8e3ac96371cf
KangHo-dev <tiger452003@gmail.com>
2024-03-09 22:00:03 +0900

    add: spring-validation exercise

---
+++ spring-validation/src/main/java/hello/itemservice/web/validation/form/ItemUpdateForm.java
@@ -0,0 +1,25 @@
+package hello.itemservice.web.validation.form;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class ItemUpdateForm {
+
+    @NotNull
+    private long id;
+
+    @NotBlank
+    private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1_000_000)
+    private Integer price;
+
+    //수정 시 수량은 자유
+    private Integer quantity;
+}
