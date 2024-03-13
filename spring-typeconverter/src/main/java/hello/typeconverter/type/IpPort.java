a9bf115c1526bfcca5c0903baa6527cdff5cb268
KangHo-dev <tiger452003@gmail.com>
2024-03-13 23:53:46 +0900

    Add: spring converter exercise

---
+++ spring-typeconverter/src/main/java/hello/typeconverter/type/IpPort.java
@@ -0,0 +1,27 @@
+package hello.typeconverter.type;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode
+public class IpPort {
+
+    private String ip;
+    private int port;
+
+    public IpPort(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
